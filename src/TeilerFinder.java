97b1f7dcfafdb5bf717f0c49faf40217b8f9bd62
GPointExtreme <dominik.geiger@hotmail.com>
2015-11-30 12:54:58 +0100

    TeilerFinder done!

---
+++ src/TeilerFinder.java
@@ -0,0 +1,17 @@
+
+public class TeilerFinder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Z = 100;
+		
+		for (int i = 2; i <= (Z/2); i++) {
+			if (Z%i==0) {
+				System.out.println(i);
+			}
+	
+		}
+		
+	}
+
+}
